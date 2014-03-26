bfd5a0fdfba7a2f983d93f65a63525494d4617da
Ole Herman <olehermanse@OHMBA.local>
2014-03-26 01:18:14 +0100

    Minor Changes

--- main.py
+++ main.py
@@ -2,38 +2,42 @@
 # -Ole Herman Schumacher Elgesem
 #  www.github.com/olehermanse
 # Simple template to get started with pygame
+# If you download the Makefile you can run by using make
 
-#Pygame is not yet updated for python 3+
-#See Makefile
+# Loosely based on the pygame cheat sheet
+#  www.inventwithpython.com/pygamecheatsheet.png
+# See www.pygame.org for more info
+# Pygame is not yet updated for python 3+
 
-#Setup
+# Setup
 import pygame, sys
 from pygame.locals import *
 pygame.init()
 fpsClock = pygame.time.Clock()
 
-#Window management
+# Window management
 windowSurfaceObj = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Player Movement in pygame')
 
-#Global variables for visuals and "game logic"
+# Global variables for visuals and "game logic"
 whiteColor = pygame.Color(255,255,255)
 blackColor = pygame.Color(0,0,0)
 playerx, playery = 0,0
-unit = 20
+unit = 40
 
-#Fill BG with black and draw player on top
+# Fill BG with black and draw player on top
 def drawScene():
 	windowSurfaceObj.fill(blackColor)
 	pygame.draw.rect(windowSurfaceObj, whiteColor, (playerx, playery, unit, unit))
 
-#Moves player by (x*unit, y*unit)
+# Moves player by (x*unit, y*unit)
 def playerMove(x,y):
 	global playerx
 	global playery
 	playerx += x*unit
 	playery += y*unit
 
+# Infinite loop can be broken by quit event
 while True:
 	#Handle events:
 	for event in pygame.event.get():
